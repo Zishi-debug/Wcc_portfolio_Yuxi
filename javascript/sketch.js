8f5c3cce7e65e35d1a9bf730212d0009fce115d6
Zishi-debug <tuzishizhenkeai@gmail.com>
2023-02-09 00:03:05 +0000

    added model

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,10 +1,24 @@
+
+let Cmc,CmcM;
+function preload(){
+    Cmc=loadModel('assets/images/C_minecraft/C_mc.obj');
+    CmcM=loadImage('assets/images/C_minecraft/c.png')
+}
 function setup() {
-    canvas= createCanvas(400, 400);
-    // canvas.position(windowWidth/2,0);
+    canvas= createCanvas(400, 400,WEBGL);
     canvas.parent("sketch-container");
+    noStroke();  
   }
   
   function draw() {
-    background(220,0);
-    circle(width/2,height/2,100);
+    orbitControl(2, 1, 0.1);
+    background(43,163,255,0);
+    //circle(width/2,height/2,100);
+    push();
+    scale(20,-20,20);
+    translate(0,-5,0);
+    texture(CmcM);
+    model(Cmc);
+    pop();
+
   }
